3cfdcda8cd18588465c25326b609fa79ccf107ae
akash-svg <akash001karim@gmail.com>
2020-12-06 15:05:56 +0600

    MediRaz

--- app/src/main/java/com/example/mediraz/MainActivity.java
+++ app/src/main/java/com/example/mediraz/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.mediraz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mediraz.fragment.HomeFragment;
 
+public class MainActivity extends AppCompatActivity {
+    private Fragment selectedFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        selectedFragment = new HomeFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.mainFragmentContainer, selectedFragment).commit();
     }
 }
