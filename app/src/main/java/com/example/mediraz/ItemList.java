14cf8b2356aba8fbf951851a9caaedcde8f66eb7
akash-svg <akash001karim@gmail.com>
2020-12-13 13:24:08 +0600

    item list updated

---
+++ app/src/main/java/com/example/mediraz/ItemList.java
@@ -0,0 +1,15 @@
+package com.example.mediraz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ItemList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+    }
+}
